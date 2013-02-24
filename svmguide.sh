3a736b35e0edf73fec2c2c7c30b105cc3799b804
Oliver Gu <odietric@gmail.com>
2013-02-24 11:52:25 +0100

    Made script more robust and readable.

--- svmguide.sh
+++ svmguide.sh
@@ -4,32 +4,34 @@
 # www.csie.ntu.edu.tw/~cjlin/papers/guide/guide.pdf and executes the second
 # command set on page 9 with the OCaml command line interface.
 
-DATA_URL="http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary"
-DATA_DIR="./data"
-SVM_CLI="./svm_cli.native"
+URL="http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary"
+DATADIR="./data"
 
-if [ ! -e "$DATA_DIR" ]
+TRAINFILE=svmguide1
+TESTFILE=svmguide1.t
+
+SVM="./svm_cli.native"
+
+if [ ! -f "$DATADIR/$TRAINFILE" ] || [ ! -f "$DATADIR/$TESTFILE" ]
 then
   printf "Downloading training and testing data..."
-  mkdir $DATA_DIR &&
-  wget -q -P $DATA_DIR $DATA_URL/svmguide1 &&
-  wget -q -P $DATA_DIR $DATA_URL/svmguide1.t
+  mkdir   -p $DATADIR &&
+  wget -q -P $DATADIR "$URL/$TRAINFILE" &&
+  wget -q -P $DATADIR "$URL/$TESTFILE"
   printf " finished!\n"
 fi
 
-if [ ! -f "$SVM_CLI" ]
+if [ ! -f "$SVM" ]
 then
-  echo "$SVM_CLI not found. Type 'make' for building it."
+  echo "$SVM not found. Type 'make' for building it."
   exit 1
 fi
 
-TRAIN_SET_FILE="$DATA_DIR/svmguide1"
-TEST_SET_FILE="$DATA_DIR/svmguide1.t"
-SCALE_PARAMS_FILE="$DATA_DIR/range1"
+TRAINFILE="$DATADIR/$TRAINFILE"
+TESTFILE="$DATADIR/$TESTFILE"
+SCALEPARAMSFILE="$DATADIR/range1"
 
-export CAML_LD_LIBRARY_PATH=$CAML_LD_LIBRARY_PATH:./_build/lib
-
-$SVM_CLI scale -l -1 -u 1 -s $SCALE_PARAMS_FILE $TRAIN_SET_FILE > $TRAIN_SET_FILE.scale &&
-$SVM_CLI scale -r $SCALE_PARAMS_FILE $TEST_SET_FILE > $TEST_SET_FILE.scale &&
-$SVM_CLI train $TRAIN_SET_FILE.scale $TRAIN_SET_FILE.scale.model &&
-$SVM_CLI predict $TEST_SET_FILE.scale $TRAIN_SET_FILE.scale.model $TEST_SET_FILE.predict
+$SVM scale -l -1 -u 1 -s $SCALEPARAMSFILE $TRAINFILE > $TRAINFILE.scale &&
+$SVM scale -r $SCALEPARAMSFILE $TESTFILE > $TESTFILE.scale &&
+$SVM train $TRAINFILE.scale &&
+$SVM predict $TESTFILE.scale $TRAINFILE.scale.model $TESTFILE.predict
